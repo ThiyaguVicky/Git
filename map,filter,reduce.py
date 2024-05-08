cf1febbc710c5996c20c2853bcb00a682bc97b8b
ThiyaguVicky <thiyagur.be@gmail.com>
2024-05-08 10:17:31 +0530

    File Uploaded

---
+++ map,filter,reduce.py
@@ -0,0 +1,34 @@
+##a=list(map(int,input().split()))
+##print(a)
+##z=map(lambda x:x**2,a)
+##print(list(z))
+
+
+##oddlis=list(map(int,input().split()))
+##def odd(a):
+##    return a%2!=0
+##x=filter(lambda x:odd(x),oddlis)
+##print(list(x))
+ 
+##fno = list(map(int, input().split()))
+##def factorial(n):
+##    if n == 0:
+##        return 1
+##    else:
+##        return n * factorial(n - 1)
+##factorials =map(lambda x:factorial(x), fno)
+##print(list(factorials))
+  
+##pno= list(map(int, input().split()))
+##def primeno(n):
+##    if n > 1:
+##        for i in range(2, (n//2)+1):
+##            if (n % i) == 0:
+##                 print(n, "is not a prime number")
+##                 break
+##            else:
+##                print(n, "is a prime number")                       
+##prime=filter(lambda x:primeno(x),pno)
+##print(list(prime))
+
+= list(map(int, input().split()))
